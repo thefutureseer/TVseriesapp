bf781a087f57287e8b6cf9ad9d7a81276b3ba197
Thefutureseer <30768475+thefutureseer@users.noreply.github.com>
2019-06-20 17:39:09 -0700

    add intro component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const getCurrentDate = () => {
-  const date = new Date();
-   return date.toDateString();
-};
-
-const greeting = <h1> Welcome to T.V. the series app <hr/> Current date is: { getCurrentDate() } </h1>
-ReactDOM.render(greeting, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
